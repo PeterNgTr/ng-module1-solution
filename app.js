c512d7c04c3f8b148f1515d4ea3eee5931327125
Thanh Trung Nguyen <thanh.nguyen-trung@hellopay.com>
2016-10-24 16:16:47 +0700

    improvement for checking logic

--- app.js
+++ app.js
@@ -8,24 +8,26 @@
 
     function LunchCheckController($scope) {
         $scope.lunchMenu = '';
-        $scope.message = 'Please enter data first';
+        $scope.message = '';
         $scope.colors ={};
         $scope.colors.current = {color: "red"};
         $scope.bordercolors = {};
         $scope.bordercolors.current = {border_color: "red"};
 
         $scope.checkLunchMenu = function () {
-            var result = $scope.lunchMenu.split(",").length;
-
-            if (result >= 1 && result <= 3) {
-                $scope.message = 'Enjoy!';
-                $scope.turnGreen();
-            } else if (result > 3) {
-                $scope.message = 'Too much!';
-                $scope.turnRed();
-            } else {
+            if ($scope.lunchMenu.length === 0 || $scope.lunchMenu === null){
                 $scope.message = 'Please enter data first';
                 $scope.turnRed();
+            } else {
+                var result = $scope.lunchMenu.split(",").length;
+
+                if (result >= 1 && result <= 3) {
+                    $scope.message = 'Enjoy!';
+                    $scope.turnGreen();
+                } else {
+                    $scope.message = 'Too much!';
+                    $scope.turnRed();
+                }
             }
 
         };
